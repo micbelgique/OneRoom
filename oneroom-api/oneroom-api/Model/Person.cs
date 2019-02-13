e229aa9ebdb107424ab5acc43d69268f59a25788
Magictintin98 <Magictintin98@users.noreply.github.com>
2019-02-13 11:08:44 +0100

    Intialize model

---
+++ oneroom-api/oneroom-api/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oneroom_api.Model
+{
+    public class Person
+    {
+        public Guid PersonId { get; set; }
+        public string Name { get; set; }
+        public List<Face> Faces { get; set; } = new List<Face>();
+
+        public Person(Person p)
+        {
+            this.Faces = p.Faces;
+            this.Name = p.Name;
+            this.PersonId = p.PersonId;
+        }
+
+        public Person()
+        {
+
+        }
+    }
+}
---
+++ oneroom-api/oneroom-api/Utilities/AvataaarsUtilities.cs
@@ -0,0 +1,23 @@
+﻿namespace oneroom_api.Utilities
+{
+    public static class AvataaarsUtilities
+    {
+        public static string ToColorAvataaars(string color)
+        {
+            switch (color)
+            {
+                case "Other":
+                case "Unknown":
+                    return "black";
+                case "Blond":
+                    return "blonde";
+                case "Red":
+                    return "auburn";
+                case "White":
+                    return "gray";
+                default:
+                    return color.ToLower();
+            }
+        }
+    }
+}
