a839b0944e86ef73ebea5ab61958907bbff9bfdc
Magictintin98 <Magictintin98@users.noreply.github.com>
2019-02-13 11:13:59 +0100

    Add dbContext with dependency injection

--- oneroom-api/oneroom-api/Startup.cs
+++ oneroom-api/oneroom-api/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using oneroom_api.Model;
 
 namespace oneroom_api
 {
@@ -26,6 +28,9 @@ namespace oneroom_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<OneRoomContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("OneRoomContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ oneroom-api/oneroom-api/data/OneRoomContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oneroom_api.Model
+{
+    public class OneRoomContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public OneRoomContext(DbContextOptions<OneRoomContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Face>()
+                .Ignore(f => f.Rectangle);
+        }
+
+        public DbSet<Face> Face { get; set; }
+    }
+}
