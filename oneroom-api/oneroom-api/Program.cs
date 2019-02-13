6c6c1e2bfec8c2ab79cdf661876d1d49ad26e787
Magictintin98 <Magictintin98@users.noreply.github.com>
2019-02-13 13:49:19 +0100

    Add Seed Date who doesn't seems to work properly :/

--- oneroom-api/oneroom-api/Program.cs
+++ oneroom-api/oneroom-api/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using oneroom_api.Model;
 
 namespace oneroom_api
 {
@@ -14,7 +17,26 @@ namespace oneroom_api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<OneRoomContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
---
+++ oneroom-api/oneroom-api/data/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace oneroom_api.Model
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new OneRoomContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<OneRoomContext>>()))
+            {
+                // Look for any Users.
+                if (context.Users.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                // Look for any Faces.
+                if (context.Face.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                Face face = new Face
+                {
+                    FaceId = new Guid(),
+                    Age = 24,
+                    IsMale = true,
+                    SmileLevel = 0,
+                    MoustacheLevel = 0.1,
+                    BeardLevel = 0.1,
+                    GlassesType = Microsoft.Azure.CognitiveServices.Vision.Face.Models.GlassesType.ReadingGlasses,
+                    BaldLevel = 0.15,
+                    HairColor = "brown",
+                    Rectangle = new System.Drawing.Rectangle { X = 237, Y = 22, Height = 172, Width = 172 }
+                };
+
+                List<Face> faces = new List<Face>();
+                faces.Add(face);
+
+                context.Face.AddRange(
+                    face
+                );
+
+                context.Users.AddRange(
+                    new User
+                    {
+                        UserId = 1,
+                        Username = "Joueur 1",
+                        UrlAvatar = "https://avatars.dicebear.com/v2/avataaars/OneRoomMale.svg?options[facialHairChance]=100&options[clothes][]=blazer&options[eyes][]=defaultValue&options[eyebrow][]=defaultValue&options[mouth][]=serious&options[skin][]=light&options[topChance]=100&options[accessories][]=round&options[accessoriesChance]=100&options[facialHair][]=magestic&options[hairColor][]=brown&options[facialHairColor][]=brown",
+                        PersonId = new Guid(),
+                        Name = "Joueur 1",
+                        Faces = faces
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
