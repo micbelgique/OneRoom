e373e24a68a82741bc9efc0291bbba73ed246882
Magictintin98 <Magictintin98@users.noreply.github.com>
2019-02-15 11:41:05 +0100

    Add Migration Delete Cascade

--- oneroom-api/oneroom-api/data/OneRoomContext.cs
+++ oneroom-api/oneroom-api/data/OneRoomContext.cs
@@ -14,12 +14,26 @@ namespace oneroom_api.Model
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Unique <Type>Id
             modelBuilder.Entity<Face>()
-                .HasIndex(f => f.FaceId);
+                .HasIndex(f => f.FaceId)
+                .IsUnique();
             modelBuilder.Entity<Group>()
-                .HasIndex(g => g.GroupId);
+                .HasIndex(g => g.GroupId)
+                .IsUnique();
             modelBuilder.Entity<User>()
-                .HasIndex(u => u.UserId);
+                .HasIndex(u => u.UserId)
+                .IsUnique();
+
+            // OnDelete = Cascade
+            modelBuilder.Entity<Face>()
+                .HasOne<User>()
+                .WithMany(u => u.Faces)
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<User>()
+                .HasOne<Group>()
+                .WithMany(g => g.Users)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
