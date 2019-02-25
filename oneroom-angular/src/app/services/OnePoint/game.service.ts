c57d85d2016af2be4606a0267787b14cbb81feaf
chevalierv <contact@chevalierv.be>
2019-02-25 09:36:20 +0100

    Update game.service.ts

--- oneroom-angular/src/app/services/OnePoint/game.service.ts
+++ oneroom-angular/src/app/services/OnePoint/game.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Game } from './model/game';
 import { Observable } from 'rxjs';
+import { EndPointGetterService } from 'src/app/utilities/end-point-getter.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class GameService {
   private gameUrl = localStorage.getItem('endpoint') + '/Games';
   private headers: HttpHeaders;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private EPGetter: EndPointGetterService) {
     this.headers = new HttpHeaders({
       'Access-Control-Allow-Origin': 'http://localhost:4200',
       'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
@@ -20,14 +21,14 @@ export class GameService {
   }
 
   getGame(groupName: string): Observable<Game>  {
-    return this.http.get<Game>(this.gameUrl + '/' + groupName, { headers: this.headers });
+    return this.http.get<Game>(this.EPGetter.getEndPoint() + '/Games/' + groupName, { headers: this.headers });
   }
 
   createGame(groupName: string) {
-    return this.http.post(this.gameUrl + '/' + groupName, { headers: this.headers });
+    return this.http.post(this.EPGetter.getEndPoint() + '/Games/' + groupName, { headers: this.headers });
   }
 
   deleteGame(groupName: string) {
-    return this.http.delete(this.gameUrl + '/' + groupName, { headers: this.headers });
+    return this.http.delete(this.EPGetter.getEndPoint() + '/Games/' + groupName, { headers: this.headers });
   }
 }
