53448d84cb027e6eead72edb24d131b7df9ee950
Nathan <nathan.pire@gmail.com>
2019-03-01 16:16:53 +0100

    Update Library for OneRoomLibrary

--- oneroom-angular/OneRoomLibrary/projects/library/src/one-room/game.service.ts
+++ oneroom-angular/OneRoomLibrary/projects/library/src/one-room/game.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Game } from './model/game';
+import { Observable } from 'rxjs';
+import { EndPointGetterService } from '../utilities/end-point-getter.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  private gameUrl = localStorage.getItem('endpoint') + '/Games';
+  private headers: HttpHeaders;
+
+  constructor(private http: HttpClient, private EPGetter: EndPointGetterService) {
+    this.headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+      'Access-Control-Allow-Headers': '*',
+    });
+  }
+  getStateGame(groupName: string): Observable<number> {
+    return this.http.get<number>(this.EPGetter.getEndPointUrl() + '/Games/' + groupName + '/State', { headers: this.headers });
+  }
+  getGames(): Observable<Game[]>  {
+    return this.http.get<Game[]>(this.EPGetter.getEndPointUrl() + '/Games/', { headers: this.headers });
+  }
+
+  getGame(groupName: string): Observable<Game>  {
+    return this.http.get<Game>(this.EPGetter.getEndPointUrl() + '/Games/' + groupName, { headers: this.headers });
+  }
+
+  nextState(groupName: string): Observable<number> {
+    return this.http.post<number>(this.EPGetter.getEndPointUrl() + '/Games/' + groupName + '/NextState', null);
+  }
+
+  createGame(game: Game) {
+    console.log(game);
+    return this.http.post(this.EPGetter.getEndPointUrl() + '/Games/', game, { headers: this.headers });
+  }
+
+  deleteGame(groupName: string) {
+    return this.http.delete(this.EPGetter.getEndPointUrl() + '/Games/' + groupName, { headers: this.headers });
+  }
+
 }
