d4f79201b785d1f8412c9e87ccc9b57eaa29c9b3
Nathan <nathan.pire@gmail.com>
2019-03-01 10:44:12 +0100

    updating the library FaceCognitive and implementing in register

--- oneroom-angular/register/src/app/settings/settings.component.ts
+++ oneroom-angular/register/src/app/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PersonGroupService } from '../services/cognitive/person-group.service';
+import { PersonGroupService } from '@oneroomic/facecognitivelibrary';
 import { MatSnackBar } from '@angular/material';
 import { GameService } from '../services/OnePoint/game.service';
 import { Game } from '../services/OnePoint/model/game';
