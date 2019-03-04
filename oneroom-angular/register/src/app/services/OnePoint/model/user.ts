d46231f48cb7237b79593b23033739192cbb90ad
Chevalier Valentin <valentin.chevalier@mic-belgique.be>
2019-03-04 16:48:21 +0100

    Update Congnitive and OneRoom Library

--- oneroom-angular/register/src/app/services/OnePoint/model/user.ts
+++ oneroom-angular/register/src/app/services/OnePoint/model/user.ts
@@ -52,9 +52,9 @@ export class User {
 
     u.urlAvatar = ( u.gender === Gender.MALE ?
     // tslint:disable-next-line:max-line-length
-    'https://avatars.dicebear.com/v2/avataaars/OneRoomMale.svg?options[clothes][]=blazer&options[eyes][]=defaultValue&options[eyebrow][]=defaultValue' :
+    'https://avatars.dicebear.com/v2/avataaars/OneRoomMale.svg?options[eyes][]=defaultValue&options[eyebrow][]=defaultValue' :
     // tslint:disable-next-line:max-line-length
-    'https://avatars.dicebear.com/v2/avataaars/HairLongFemale.svg?options[clothes][]=blazer&options[eyes][]=defaultValue&options[eyebrow][]=defaultValue' );
+    'https://avatars.dicebear.com/v2/avataaars/HairLongFemale.svg?options[eyes][]=defaultValue&options[eyebrow][]=defaultValue' );
 
     u.urlAvatar += u.skinColor.toLowerCase() === 'caucasian' ?
     '&options[skin][]=pale' : u.skinColor.toLowerCase() === 'black' ?
@@ -67,6 +67,40 @@ export class User {
     u.glassesType === GlassesType.Sunglasses ?
     '&options[accessories][]=sunglasses&options[accessoriesChance]=100' : '&options[accessoriesChance]=0';
 
+    const randClothes = Math.random();
+    if ( randClothes <= 1 / 6) {
+      u.urlAvatar += '&options[clothes][]=blazer';
+    } else if ( randClothes <= 1 / 3) {
+      u.urlAvatar += '&options[clothes][]=sweater';
+    } else if ( randClothes <= 1 / 2) {
+      u.urlAvatar += '&options[clothes][]=shirt';
+    } else if ( randClothes <= 2 / 3) {
+      u.urlAvatar += '&options[clothes][]=hoodie';
+    } else if ( randClothes <= 5 / 6) {
+      u.urlAvatar += '&options[clothes][]=overall';
+    } else if ( randClothes <= 1) {
+      u.urlAvatar += '&options[clothes][]=sweater&options[clothes][]=shirt&options[clothes][]=hoodie';
+    }
+
+    const randClothesColor = Math.random();
+    if ( randClothesColor <= 1 / 8) {
+      u.urlAvatar += '&options[clothesColor][]=black';
+    } else if ( randClothesColor <= 1 / 4) {
+      u.urlAvatar += '&options[clothesColor][]=blue';
+    } else if ( randClothesColor <= 3 / 8) {
+      u.urlAvatar += '&options[clothesColor][]=gray';
+    } else if ( randClothesColor <= 2 / 4) {
+      u.urlAvatar += '&options[clothesColor][]=heather';
+    } else if ( randClothesColor <= 5 / 8) {
+      u.urlAvatar += '&options[clothesColor][]=pastel';
+    } else if ( randClothesColor <= 3 / 4) {
+      u.urlAvatar += '&options[clothesColor][]=pink';
+    } else if ( randClothesColor <= 7 / 8) {
+      u.urlAvatar += '&options[clothesColor][]=red';
+    } else if ( randClothesColor <= 1) {
+      u.urlAvatar += '&options[clothesColor][]=white';
+    }
+
     if (u.gender === Gender.MALE) {
       u.urlAvatar += u.beardLevel > 0.5 ?
         '&options[facialHair][]=medium' : u.beardLevel > 0.25 ?
@@ -76,6 +110,63 @@ export class User {
     } else {
       u.urlAvatar += '&options[facialHairChance]=0';
     }
+
+    const randHairLength = Math.random();
+    switch (u.hairLength) {
+      case 'short':
+        if (u.gender === Gender.FEMALE) {
+          if ( randHairLength <= 1 / 3 ) {
+            u.urlAvatar += '&options[top][]=shortHair&options[top][]=hat';
+          } else if ( randHairLength <= 2 / 3 ) {
+            u.urlAvatar += '&options[top][]=shortHair&options[top][]=turban';
+          } else if ( randHairLength <= 1 ) {
+            u.urlAvatar += '&options[top][]=shortHair&options[top][]=turban&options[top][]=hijab';
+          }
+        } else {
+          if ( randHairLength <= 1 / 4) {
+            u.urlAvatar += '&options[top][]=shortHair';
+          } else if ( randHairLength <= 1 / 2) {
+            u.urlAvatar += '&options[top][]=shortHair&options[top][]=hat';
+          } else if ( randHairLength <= 3 / 4) {
+            u.urlAvatar += '&options[top][]=shortHair&options[top][]=longHair&options[top][]=hat&options[top][]=hijab';
+          } else if ( randHairLength <= 1 ) {
+            u.urlAvatar += '&options[top][]=shortHair&options[top][]=longHair&options[top][]=hat';
+          }
+        }
+        break;
+      case 'mid':
+        if (u.gender === Gender.FEMALE) {
+          if ( randHairLength <= 1 / 3) {
+            u.urlAvatar += '&options[top][]=shortHair';
+          } else if ( randHairLength <= 2 / 3) {
+            u.urlAvatar += '&options[top][]=longHair&options[top][]=shortHair';
+          } else if ( randHairLength <= 1 ) {
+            u.urlAvatar += '&options[top][]=longHair&options[top][]=eyepatch';
+          }
+        } else {
+          if ( randHairLength <= 1 / 2) {
+            u.urlAvatar += '&options[top][]=longHair';
+          } else if ( randHairLength <= 1 ) {
+            u.urlAvatar += '&options[top][]=longHair&options[top][]=eyepatch';
+          }
+        }
+        break;
+      case 'long':
+        if (u.gender === Gender.FEMALE) {
+          if ( randHairLength <= 1 / 2) {
+            u.urlAvatar += '&options[top][]=longHair&options[top][]=shortHair&options[top][]=hat';
+          }
+          // Else hair length already present in the seed
+        } else {
+          if ( randHairLength <= 1 / 2) {
+            u.urlAvatar += '&options[top][]=longHair&options[top][]=hat&options[top][]=hijab&options[top][]=turban';
+          } else if ( randHairLength <= 1 ) {
+            u.urlAvatar += '&options[top][]=longHair&options[top][]=hat';
+          }
+        }
+        break;
+    }
+
       // hair color
     u.urlAvatar += '&options[hairColor][]=';
     u.urlAvatar += u.hairColor.toLowerCase() === 'other' ?
