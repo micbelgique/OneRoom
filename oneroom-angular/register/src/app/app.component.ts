377cbd919b4bca232f89dc96c3a1c92c932b886a
Nathan <nathan.pire@gmail.com>
2019-03-12 10:09:24 +0100

    little edits to remove preview results inside register and customvision

--- oneroom-angular/register/src/app/app.component.ts
+++ oneroom-angular/register/src/app/app.component.ts
@@ -19,19 +19,19 @@ export class AppComponent {
 
   constructor() {
     // tslint:disable-next-line:max-line-length
-    localStorage.setItem('endpointVisionPrediction', 'https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/aa05f819-5d11-453f-9603-88e919abb71d/image');
-    localStorage.setItem('keyVisionPrediction', 'f011cedabc6749a6bebd8cede806948d');
+    localStorage.setItem('endpointVisionPrediction', 'https://westeurope.api.cognitive.microsoft.com/customvision/v2.0/Prediction/a1cb0694-4bdb-4def-a20f-52226ced6ded/image');
+    localStorage.setItem('keyVisionPrediction', '8139b0c8c2a54b59861bbe5e7e089d2b');
     // tslint:disable-next-line:max-line-length
-    localStorage.setItem('endpointVisionTraining', 'https://southcentralus.api.cognitive.microsoft.com/customvision/v2.2/Training/projects/aa05f819-5d11-453f-9603-88e919abb71d/predictions');
-    localStorage.setItem('keyVisionTraining', 'cc45e01b8c6f4e438411f339a78ead09');
+    localStorage.setItem('endpointVisionTraining', 'https://westeurope.api.cognitive.microsoft.com/customvision/v2.2/Training/projects/a1cb0694-4bdb-4def-a20f-52226ced6ded/predictions');
+    localStorage.setItem('keyVisionTraining', '3e55ee24eb094201bc29899a1f515843');
 
     // tslint:disable-next-line:max-line-length
-    localStorage.setItem('endpointHairLengthPrediction', 'https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/7e0f5d3c-58cd-45ba-bd1e-e53e26e0e72b/image');
-    localStorage.setItem('keyHairLengthPrediction', 'f011cedabc6749a6bebd8cede806948d');
+    localStorage.setItem('endpointHairLengthPrediction', 'https://westeurope.api.cognitive.microsoft.com/customvision/v2.0/Prediction/3ae9a19d-fa15-4b44-bfb5-b02bb11b3efc/image');
+    localStorage.setItem('keyHairLengthPrediction', '8139b0c8c2a54b59861bbe5e7e089d2b');
 
-    localStorage.setItem('endpointHairLengthTraining', 'https://southcentralus.api.cognitive.microsoft.com/customvision/v2.2/Training/');
-    localStorage.setItem('keyHairLengthTraining', 'cc45e01b8c6f4e438411f339a78ead09');
-    localStorage.setItem('projectIdHairLength', '7e0f5d3c-58cd-45ba-bd1e-e53e26e0e72b');
+    localStorage.setItem('endpointHairLengthTraining', 'https://westeurope.api.cognitive.microsoft.com/customvision/v2.2/Training/');
+    localStorage.setItem('keyHairLengthTraining', '3e55ee24eb094201bc29899a1f515843');
+    localStorage.setItem('projectIdHairLength', '3ae9a19d-fa15-4b44-bfb5-b02bb11b3efc');
 
   }
 
