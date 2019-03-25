bdd750465ff4bb85062e3f6b1c72a24b452e3107
Chevalier Valentin <valentin.chevalier@mic-belgique.be>
2019-03-25 10:34:14 +0100

    Add Scenario

---
+++ oneroom-api/oneroom-api/Model/Scenario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oneroom_api.Model
+{
+    public class Scenario
+    {
+        public int ScenarioId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<ScenarioChallenge> ScenarioChallenges { get; set; } = new List<ScenarioChallenge>();
+    }
+}
