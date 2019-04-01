821843f93f106b52289810dfdf12723de7841890
Nathan <nathan.pire@gmail.com>
2019-04-01 15:32:01 +0200

    Creating VaultController and function on Hub

---
+++ oneroom-api/oneroom-api/Controllers/VaultController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using oneroom_api.Hubs;
+using oneroom_api.Model;
+
+namespace oneroom_api.Controllers
+{
+    [Route("api/[controller]/{gameId}/{teamId}")]
+    [ApiController]
+    public class VaultController : ControllerBase
+    {
+        private readonly OneRoomContext _context;
+        private readonly IHubContext<OneHub, IActionClient> _hubClients;
+
+        public VaultController(
+            OneRoomContext context,
+            IHubContext<OneHub, IActionClient> hubClients)
+        {
+            _context = context;
+            _hubClients = hubClients;
+        }
+
+        [HttpGet]
+        public async void FinishGame(int teamId,int gameId)
+        {
+            await _hubClients.Clients.Group(gameId.ToString()).FinishGame(teamId);
+        }
+    }
+}
