6515bd90a88bca02832ceca76c101d8c009e44a9
Rossi Loris <rossi.loris1998@gmail.com>
2019-04-04 10:48:54 +0200

    Launcher : Disabling setup when game is launched

--- oneroom-angular/launcher/src/app/settings/settings.component.ts
+++ oneroom-angular/launcher/src/app/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
-import { Game, GameService } from '@oneroomic/oneroomlibrary';
+import { Game, GameService, LeaderboardService, GameState } from '@oneroomic/oneroomlibrary';
 import { Router } from '@angular/router';
 
 export enum KEY_CODE {
@@ -24,6 +24,10 @@ export class SettingsComponent implements OnInit {
     // game
     game: Game;
     games: Game[];
+      // signalR
+    private hubServiceSub;
+    private gameSub;
+    infoMessage: string;
     // Face
     subscriptionKey: string;
     endPointCognitive: string;
@@ -43,14 +47,36 @@ export class SettingsComponent implements OnInit {
     constructor(
       private toast: MatSnackBar,
       private gameService: GameService,
-      private router: Router
+      private router: Router,
+      private hubService: LeaderboardService
       ) {}
 
     ngOnInit() {
+      this.infoMessage = null;
       this.games = [];
       // game
       if (localStorage.getItem('gameData')) {
         this.game = JSON.parse(localStorage.getItem('gameData'));
+
+        // DISABLE CONFIG WHEN GAME LAUNCHED
+
+        // join new group
+        this.hubServiceSub = this.hubService.run().subscribe(
+          () => this.hubService.joinGroup(this.game.gameId.toString())
+        );
+
+        // new game state
+        this.gameSub = this.hubService.refreshGameState.subscribe(
+        (gameId) => {
+          if (gameId === this.game.gameId) {
+            this.refreshGameState(this.game);
+          }
+        },
+        (err) => {
+          console.log(err);
+        });
+
+        this.refreshGameState(this.game);
       } else {
         this.game = new Game();
         this.game.groupName = null;
@@ -167,6 +193,23 @@ export class SettingsComponent implements OnInit {
           duration: 2000
         });
       }
+    }
+
+     /* Update the game state to disable configuration modifications */
+     refreshGameState(game: Game) {
+      const res$ = this.gameService.getStateGame(game.groupName);
+      res$.subscribe(
+        (state) => {
+          if (state === GameState.LAUNCH) {
+            this.infoMessage = 'La partie est lancée, les paramètres ne peuvent plus être modifié !';
+          } else {
+            this.infoMessage = null;
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
     }
 
 }
