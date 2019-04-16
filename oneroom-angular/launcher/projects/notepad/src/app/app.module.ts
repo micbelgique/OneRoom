e3cf628db0872fe11775f3f6f740e0bff341fe31
Nathan <nathan.pire@gmail.com>
2019-04-16 11:05:25 +0200

    finishing Notepad

--- oneroom-angular/launcher/projects/notepad/src/app/app.module.ts
+++ oneroom-angular/launcher/projects/notepad/src/app/app.module.ts
@@ -4,6 +4,30 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import {
+  MatChipsModule,
+  MatSliderModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatTabsModule,
+  MatMenuModule,
+  MatSlideToggleModule,
+  MatSidenavModule,
+  MatProgressBarModule,
+  MatSnackBarModule,
+  MatDialogModule,
+  MatSelectModule,
+  MatDatepickerModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 const arrProviders = [];
 
@@ -12,7 +36,30 @@ const arrProviders = [];
 
   HomeComponent],
   imports: [
-
+    MatSliderModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    FormsModule,
+    BrowserModule,
+    MatChipsModule
   ],
   exports: [
 
--- oneroom-angular/launcher/projects/notepad/src/app/home/home.component.ts
+++ oneroom-angular/launcher/projects/notepad/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from './note';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  notes: Note[] = localStorage.getItem('notes') != null ? JSON.parse(localStorage.getItem('notes')) : [];
+  currentTitle = '';
+  currentDescription = '';
+  options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   constructor() { }
 
   ngOnInit() {
+    console.log(this.notes);
   }
-
+  addNote() {
+    const newNote = new Note();
+    newNote.date = new Date().toLocaleDateString('fr-FR', this.options);
+    newNote.title = this.currentTitle;
+    newNote.description = this.currentDescription;
+    this.notes.push(newNote);
+    this.currentTitle = '';
+    this.currentDescription = '';
+    console.log(JSON.stringify(this.notes));
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+  removeNote(num: number) {
+    this.notes.splice(num, 1);
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnDestroy() {
+    console.log('destroy note');
+  }
 }
