4b955b26b41c34477e97d6bd46dbb02e9a99f1e0
Rossi Loris <rossi.loris1998@gmail.com>
2019-04-29 21:18:36 +0200

    Adding models for speech2text & text2speech

--- oneroom-angular/FaceCognitiveLibrary/projects/library/src/cognitive/voice/voice-models.ts
+++ oneroom-angular/FaceCognitiveLibrary/projects/library/src/cognitive/voice/voice-models.ts
@@ -1,5 +1,22 @@
-// TODO : voice models
+// speech to text google
 
 export class Alternative {
+  alternatives: Transcription[];
 
+}
+
+export class Transcription {
+  confidence: number;
+  transcript: string;
+}
+
+export class SpeechToTextResponse {
+  results: Alternative[];
+}
+
+
+// text to speech
+
+export class TextToSpeechResponse {
+  audioContent: string;
 }
