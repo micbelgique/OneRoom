0bf3bb2e899f0ae9c668cc45d3de311c1997f52f
Nathan <nathan.pire@gmail.com>
2019-05-06 16:16:39 +0200

    Selecting skinColor

--- oneroom-angular/padlock/src/app/home/home.component.ts
+++ oneroom-angular/padlock/src/app/home/home.component.ts
@@ -43,18 +43,46 @@ export class HomeComponent implements OnInit {
     switch (best) {
       case 0:
         this.UserWanted.hairColor = 'blond';
+        if (this.team.users.filter(u => u.hairColor === 'blond' && u.skinColor === 'black').length >= 1) {
+          this.UserWanted.skinColor = 'black';
+        } else if (this.team.users.filter(u => u.hairColor === 'blond' && u.skinColor === 'azian').length >= 1) {
+          this.UserWanted.skinColor = 'azian';
+        } else {
+          this.UserWanted.skinColor = 'caucasian';
+        }
         console.log(this.UserWanted);
         break;
       case 1:
       this.UserWanted.hairColor = 'brown';
+      if (this.team.users.filter(u => u.hairColor === 'brown' && u.skinColor === 'black').length >= 1) {
+        this.UserWanted.skinColor = 'black';
+      } else if (this.team.users.filter(u => u.hairColor === 'brown' && u.skinColor === 'azian').length >= 1) {
+        this.UserWanted.skinColor = 'azian';
+      } else {
+        this.UserWanted.skinColor = 'caucasian';
+      }
       console.log(this.UserWanted);
       break;
       case 2:
       this.UserWanted.hairColor = 'red';
+      if (this.team.users.filter(u => u.hairColor === 'red' && u.skinColor === 'black').length >= 1) {
+        this.UserWanted.skinColor = 'black';
+      } else if (this.team.users.filter(u => u.hairColor === 'red' && u.skinColor === 'azian').length >= 1) {
+        this.UserWanted.skinColor = 'azian';
+      } else {
+        this.UserWanted.skinColor = 'caucasian';
+      }
       console.log(this.UserWanted);
       break;
       case 3:
       this.UserWanted.hairColor = 'white';
+      if (this.team.users.filter(u => u.hairColor === 'white' && u.skinColor === 'black').length >= 1) {
+        this.UserWanted.skinColor = 'black';
+      } else if (this.team.users.filter(u => u.hairColor === 'white' && u.skinColor === 'azian').length >= 1) {
+        this.UserWanted.skinColor = 'azian';
+      } else {
+        this.UserWanted.skinColor = 'caucasian';
+      }
       console.log(this.UserWanted);
       break;
       default:
