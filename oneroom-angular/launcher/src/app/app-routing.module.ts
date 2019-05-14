294be63a69c5f0d7dbe67d08ab198a9034678cb9
Nathan <nathan.pire@gmail.com>
2019-05-14 15:04:33 +0200

    branding onepoint on launcher and adding browser to onepoint website

--- oneroom-angular/launcher/src/app/app-routing.module.ts
+++ oneroom-angular/launcher/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { NotepadSharedModule } from 'projects/notepad/src/app/app.module';
 import { BatterySharedModule } from 'projects/battery/src/app/app.module';
 import { RadioSharedModule } from 'projects/radio/src/app/app.module';
 import { ClockAppSharedModule } from 'projects/clock/src/app/app.module';
+import { BrowserAppSharedModule } from 'projects/browser/src/app/app.module';
 
 const routes: Routes = [
   {path: 'scanner',
@@ -36,6 +37,8 @@ const routes: Routes = [
     loadChildren: '../../projects/radio/src/app/app.module#RadioSharedModule'},
   { path: 'clock',
     loadChildren: '../../projects/clock/src/app/app.module#ClockAppSharedModule'},
+  { path: 'browser',
+    loadChildren: '../../projects/browser/src/app/app.module#BrowserAppSharedModule'},
   { path: 'nav', component: NavComponent },
   { path: 'lock', component: LockscreenComponent },
   { path: 'settings', component : SettingsComponent},
@@ -55,7 +58,8 @@ const routes: Routes = [
     NotepadSharedModule.forRoot(),
     BatterySharedModule.forRoot(),
     RadioSharedModule.forRoot(),
-    ClockAppSharedModule.forRoot()
+    ClockAppSharedModule.forRoot(),
+    BrowserAppSharedModule.forRoot()
   ],
   exports: [RouterModule]
 })
